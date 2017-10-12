cddcd6700772af7dc29103eacead03d32788bcc6
Vlad Bogdan <vlad.bogdan90@gmail.com>
2017-10-12 23:47:50 +0200

    Added some functionality(work in progress)

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-function getButtonsValue() {
+var calculator = (function() {
+
     var btns = document.getElementsByTagName('button');
     for (var i = 0; i < btns.length; i++) {
-        btns[i].onclick = value;
+        btns[i].onclick = btnsValue;
     }
 
-    function value() {
-    	this.getAttribute('value');
+    function btnsValue(e) {
+        // Get the input and button values
+        var input = document.querySelector('.screen');
+        var inputVal = input.innerHTML;
+        var btnVal = this.getAttribute('value');
+       	
+        if (btnVal == 'C') {
+            input.innerHTML = '';
+
+        } else if (btnVal == '=') {
+            var equation = inputVal;
+
+            equation = equation.replace(/×/g, '*').replace(/÷/g, '/');
+
+            input.innerHTML = eval(equation);
+            
+        } else {
+        	input.innerHTML += btnVal;
+        }
+
+        // prevent page jumps
+        e.preventDefault();
     }
-}
 
-getButtonsValue();
+})();
 
 
-//document.querySelector('.display').innerHTML =  
+
+
